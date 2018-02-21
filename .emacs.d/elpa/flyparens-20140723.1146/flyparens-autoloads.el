93bd08c01a0844e18b6c1cbbcde181d3189a4f7c
ninjha01@gmail.com <ninjha01@gmail.com>
2018-02-20 19:55:47 -0500

    Deleting modes

--- .emacs.d/elpa/flyparens-20140723.1146/flyparens-autoloads.el
+++
@@ -1,26 +0,0 @@
-;;; flyparens-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
-
-;;;### (autoloads nil "flyparens" "flyparens.el" (22707 18302 0 0))
-;;; Generated autoloads from flyparens.el
-
-(autoload 'flyparens-mode "flyparens" "\
- FlyParens
-
-\(fn &optional ARG)" t nil)
-
-;;;***
-
-;;;### (autoloads nil nil ("flyparens-config-example-1.el" "flyparens-pkg.el")
-;;;;;;  (22707 18302 0 0))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; flyparens-autoloads.el ends here
--- .emacs.d/elpa/smooth-scroll-20130321.2114/smooth-scroll-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "smooth-scroll" "20130321.2114" "Minor mode for smooth scrolling and in-place scrolling." 'nil :commit "02320f28abb5cae28b3a18f6b9ce93129bdbfc45" :url "http://www.emacswiki.org/emacs/download/smooth-scroll.el" :keywords '("convenience" "emulations" "frames"))
