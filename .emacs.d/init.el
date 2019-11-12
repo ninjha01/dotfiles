438cac0ce95beb7f639125a05e12fd75992d02f7
Nishant Jha <ninjha01@gmail.com>
2019-11-12 17:01:23 -0500

    Update init.el

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -60,6 +60,10 @@
 
 ;; Behavior Modification ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
+;; God mode
+(require 'god-mode)
+(global-set-key (kbd "<escape>") 'god-local-mode)
+
 ;; Ivy
 (ivy-mode 1)
 (setq ivy-use-virtual-buffers t)
