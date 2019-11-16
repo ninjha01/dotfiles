2381f55bca21f842c748ed161749c44391bced3f
Nishant Jha <ninjha01@gmail.com>
2019-11-16 07:31:14 -0500

    Style refactor

---
+++ .emacs.d/elisp/style.el
@@ -0,0 +1,32 @@
+(setq inhibit-startup-screen t)
+(set-face-attribute 'region nil :background "#666" :foreground "#ffffff")
+
+;;Clear Scratch
+(setq initial-scratch-message "")
+
+;; Custom
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-enabled-themes (quote (zenburn)))
+ '(custom-safe-themes
+   (quote
+    ("acaccddbc0ae7d5c2cdea2e64b0261ca383671205752c062c44590d944ad0842" default)))
+ '(package-selected-packages
+   (quote
+    (prettier-js tide js-comint flycheck-rust beacon ace-window magit magit-topgit yaml-mode which-key web-mode vlf use-package smooth-scroll rust-mode multiple-cursors markdown-mode helm-flycheck flyparens company-web company-shell company-go)))
+ '(vlf-application (quote dont-ask)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+
+;; EWW
+;; Hide all images
+(setq shr-inhibit-images t)
+;; set background color to servicable gray
+(setq shr-color-visible-luminance-min 70)
--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -6,6 +6,9 @@
 ;;                                                               ;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
+(add-to-list 'load-path "~/.emacs.d/elisp/")
+(load-library "style")
+
 ;; Init ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Package system
 (require 'package)
@@ -26,37 +29,9 @@
 (mood-line-mode)
 (tool-bar-mode -1)
 (scroll-bar-mode -1)
-(setq inhibit-startup-screen t)
-(set-face-attribute 'region nil :background "#666" :foreground "#ffffff")
 
-;;Clear Scratch
-(setq initial-scratch-message "")
+;; Style
 
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(custom-enabled-themes (quote (zenburn)))
- '(custom-safe-themes
-   (quote
-    ("acaccddbc0ae7d5c2cdea2e64b0261ca383671205752c062c44590d944ad0842" default)))
- '(package-selected-packages
-   (quote
-    (prettier-js tide js-comint flycheck-rust beacon ace-window magit magit-topgit yaml-mode which-key web-mode vlf use-package smooth-scroll rust-mode multiple-cursors markdown-mode helm-flycheck flyparens company-web company-shell company-go)))
- '(vlf-application (quote dont-ask)))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
-
-;; EWW
-;; Hide all images
-(setq shr-inhibit-images t)
-;; set background color to servicable gray
-(setq shr-color-visible-luminance-min 70)
 
 ;; Behavior Modification ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
