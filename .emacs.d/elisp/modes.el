60e9819a8c515a071b112056248132a03092416c
Nishant Jha <ninjha01@gmail.com>
2019-11-19 09:03:35 -0500

    add magit to package download

--- .emacs.d/elisp/modes.el
+++ .emacs.d/elisp/modes.el
@@ -5,7 +5,7 @@
              '("melpa" . "https://melpa.org/packages/"))
 (package-initialize)
 
-(setq package-list '(god-mode todoist docker projectile browse-kill-ring counsel dumb-jump ivy mood-line elpy beacon ace-window which-key company rust-mode yaml-mode web-mode vlf smooth-scroll multiple-cursors markdown-mode helm-flycheck company-web company-shell company-go))
+(setq package-list '(magit god-mode todoist docker projectile browse-kill-ring counsel dumb-jump ivy mood-line elpy beacon ace-window which-key company rust-mode yaml-mode web-mode vlf smooth-scroll multiple-cursors markdown-mode helm-flycheck company-web company-shell company-go))
 (dolist (package package-list)
   (unless (package-installed-p package)
     (package-install package)))
