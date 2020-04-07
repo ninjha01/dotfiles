38dcbd10b6675c7a5d4f842f6b4aa3d9bcfb3320
ninjha01 <ninjha01@gmail.com>
2020-04-07 11:53:25 -0400

    typo fix

--- .emacs.d/elisp/modes.el
+++ .emacs.d/elisp/modes.el
@@ -254,7 +254,7 @@
 (setq gc-cons-threshold 100000000)
 ;; "Increase the amount of data which Emacs reads from the process. Again the emacs default is too low 4k considering that the some of the language server responses are in 800k - 3M range."
 (setq read-process-output-max (* 1024 1024))
-
+(require 'company)
 (push 'company-lsp company-backends)
 
 
