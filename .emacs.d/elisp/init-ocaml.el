f60059084e3b9829da31d265d25aefdeaf2a229a
Nishant Jha <me@nishantjha.org>
2021-02-22 22:33:45 -0500

    ocaml tuareg

--- .emacs.d/elisp/init-ocaml.el
+++ .emacs.d/elisp/init-ocaml.el
@@ -12,7 +12,8 @@
   (when (eq major-mode 'tuareg-mode) 
     (ocp-indent-buffer) 
     (ocamlformat)))
-
+(require 'tuareg)
+(define-key tuareg-mode-map (kbd "C-c C-c") 'tuareg-eval-buffer)
 (add-hook 'before-save-hook #'my-tuareg-mode-before-save-hook)
 ;; ## added by OPAM user-setup for emacs / base ## 56ab50dc8996d2bb95e7856a6eddb17b ## you can edit, but keep this line
 (require 'opam-user-setup "~/.emacs.d/opam-user-setup.el")
