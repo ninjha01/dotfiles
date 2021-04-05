da6212edc2e5195d0b58fa00e4010c26cc47ee21
ninjha01 <ninjha01@gmail.com>
2021-04-05 09:32:25 -0400

    Revert "update workon home"
    This reverts commit 4baee9befdf036dac744bc00d6a2f439ed1fc680.

--- .emacs.d/elisp/init-python.el
+++ .emacs.d/elisp/init-python.el
@@ -11,8 +11,8 @@
 ;;;; elpy
 (setq python-shell-interpreter "python3" elpy-rpc-python-command "python3"
       python-shell-interpreter-args "-i")
-(setenv "WORKON_HOME" "/usr/local/Caskroom/miniconda/base/envs/")
 (setq flycheck-flake8rc "~/dotfiles/python/.flake8")
+(setenv "WORKON_HOME" "/Users/njha/miniconda3/envs/")
 (require 'elpy)
 (add-hook 'elpy-mode-hook 
 	  '(lambda () 
