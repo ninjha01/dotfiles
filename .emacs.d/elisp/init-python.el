84acd84eb6c7970d54746033813d404a44174e8a
Nishant Jha <me@nishantjha.org>
2021-06-23 13:21:10 -0400

    use jedi for gotodefinition

--- .emacs.d/elisp/init-python.el
+++ .emacs.d/elisp/init-python.el
@@ -23,14 +23,14 @@
   (add-hook 'elpy-mode-hook 'flycheck-mode))
 (defalias 'workon 'pyvenv-workon)
 
-(defun elpy-goto-definition-or-rgrep ()
+(defun jedi-goto-definition-or-rgrep ()
   "Go to the definition of the symbol at point, if found. Otherwise, run `elpy-rgrep-symbol'."
     (interactive)
     (ring-insert find-tag-marker-ring (point-marker))
-    (condition-case nil (elpy-goto-definition)
+    (condition-case nil (jedi:goto-definition)
         (error (elpy-rgrep-symbol
                 (concat "\\(def\\|class\\)\s" (thing-at-point 'symbol) "(")))))
-(define-key elpy-mode-map (kbd "M-.") 'elpy-goto-definition-or-rgrep)
+(define-key elpy-mode-map (kbd "M-.") 'jedi-goto-definition-or-rgrep)
 
 
 (provide 'init-python)
