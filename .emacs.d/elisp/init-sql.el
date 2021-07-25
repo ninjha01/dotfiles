93908db5227a4ac3eddb08308893377c6f0e29c7
Nishant Jha <me@nishantjha.org>
2021-07-25 16:00:42 -0400

    add sql config

--- .emacs.d/elisp/init-sql.el
+++ .emacs.d/elisp/init-sql.el
@@ -5,5 +5,6 @@
 
 
 (setq sqlformat-command 'pgformatter)
-(setq sqlformat-args '("-s2" "-g"))
+(setq sqlformat-args '("-s2" "-u1" "-U1" "-W5"))
 (add-hook 'sql-mode-hook 'sqlformat-on-save-mode)
+(provide 'init-sql)
