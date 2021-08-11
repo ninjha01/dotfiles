e78dd2c5f692321ce0447a282ab43704f4826152
Nishant Jha <me@nishantjha.org>
2021-08-11 15:59:48 -0400

    some new magit keybindings

--- .emacs.d/elisp/init-git.el
+++ .emacs.d/elisp/init-git.el
@@ -5,13 +5,10 @@
 
 (global-set-key (kbd "C-x g") 'magit-status)
 (global-set-key (kbd "C-c g") 'magit-file-dispatch)
+(global-set-key (kbd "C-c d") 'magit-dispatch)
+(global-set-key (kbd "C-c b") 'magit-blame)
 (with-eval-after-load 'magit-mode (add-hook 'after-save-hook 'magit-after-save-refresh-status t)) ;; auto-refresh
 
-(defun git-add-current-buffer () 
-  "Adds (with force) the file from the current buffer to the git repo" 
-  (interactive) 
-  (shell-command (concat "git add -f " (shell-quote-argument buffer-file-name)))
-  )
 
 (with-eval-after-load 'magit 
   (require 'forge))
