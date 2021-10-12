5415276c9a34c2df30b973f77bc400822e38937e
Nishant Jha <nishantjha@Nishants-MacBook-Pro.local>
2021-10-12 18:15:09 -0400

    update and reorder brewfile and call from install script

--- macOS_install.sh
+++ macOS_install.sh
@@ -1,51 +1,9 @@
 #!/bin/bash
+set -xe
 xcode-select --install
 
 if ! type -P brew; then
     /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 fi
 
-brew install pandoc \
-     node \
-     ispell \
-     imagemagick \
-     awscli \
-     eslint \
-     ffmpeg \
-     gnutls \
-     prettier \
-     redis \
-     sqlite \
-     the_silver_searcher \
-     watchman \
-     yarn \
-     youtube-dl \
-     shellcheck \
-     proselint \
-     wget \
-     postgresql \
-     proselint
-
-brew cask install \
-     miniconda \
-     adoptopenjdk8 \
-     aerial \
-     alfred \
-     android-studio \
-     bartender \
-     calibre \
-     chromedriver \
-     docker \
-     emacs \
-     google-backup-and-sync \
-     google-chrome \
-     google-cloud-sdk \
-     itsycal \
-     keepassxc \
-     microsoft-office \
-     spectacle \
-     transmission \
-     xquartz \
-     db-browser-for-sqlite \
-     slack \
-     brightness \
+brew bundle
