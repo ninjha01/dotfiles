441bf0d47c72a3f61c5b70c021887165a7f538fc
Nishant Jha <nishantjha@Nishants-MacBook-Pro.local>
2021-10-22 14:53:51 -0400

    disable font smoothing

--- macOS_install.sh
+++ macOS_install.sh
@@ -2,6 +2,7 @@
 set -xe
 xcode-select --install
 
+# Install brew
 if ! type -P brew; then
     /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 fi
@@ -9,3 +10,7 @@ fi
 brew bundle
 
 ./symlink.sh
+
+# Disable font smoothing (might?) require reboot
+# from https://tonsky.me/blog/monitors/
+defaults -currentHost write -g AppleFontSmoothing -int 0
