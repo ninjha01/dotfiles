9b922aa0fb6f8304595c6af30f103ee5bf95c841
Nishant <nishantjha@Nishants-MacBook-Pro-2.local>
2021-12-06 14:32:12 -0500

    use pyright lsp

--- .emacs.d/elisp/custom.el
+++ .emacs.d/elisp/custom.el
@@ -5,8 +5,7 @@
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
    '("95d0ed21bb0e919be7687a25ad59a1c2c8df78cbe98c9e369d44e65bfd65b167" "acaccddbc0ae7d5c2cdea2e64b0261ca383671205752c062c44590d944ad0842" "7b3d184d2955990e4df1162aeff6bfb4e1c3e822368f0359e15e2974235d9fa8" "0a41da554c41c9169bdaba5745468608706c9046231bbbc0d155af1a12f32271" "e6ff132edb1bfa0645e2ba032c44ce94a3bd3c15e3929cdf6c049802cf059a2a" default))
- '(package-selected-packages
-   '()))
+ '(package-selected-packages nil))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
--- .emacs.d/elisp/init-lsp.el
+++ .emacs.d/elisp/init-lsp.el
@@ -7,28 +7,31 @@
 
 (use-package 
   lsp-mode 
-  :config (setq read-process-output-max (* 1024 1024) lsp-idle-delay 0.5 ;; "Increase the amount of data which Emacs reads from the process. Again the emacs default is too low 4k considering that the some of the language server responses are in 800k - 3M range."
-		lsp-enable-symbol-highlighting t lsp-enable-snippet nil	;; Not supported by company capf, which is the recommended company backend
-		lsp-pyls-plugins-flake8-enabled t gc-cons-threshold 100000000 ;; "Adjust gc-cons-threshold. The default setting is too low for lsp-mode’s needs due to the fact that client/server communication generates a lot of memory/garbage."
-		lsp-headerline-breadcrumb-enable nil ;; Don't show headerline
-		lsp-completion-show-kind nil ;; Don't show completion kind
-		lsp-completion-show-detail nil ;; Don't show Completion item detail
-		) 
-  (lsp-register-custom-settings '(
-				  ;; Python
-				  ("pyls.plugins.pyls_mypy.enabled" t t) 
-				  ("pyls.plugins.pyls_mypy.live_mode" nil t) 
-				  ("pyls.plugins.pyls_black.enabled" t t) 
-				  ("pyls.plugins.pyls_isort.enabled" t t)
-				  ;; Disable these as they're duplicated by flake8
-				  ("pyls.plugins.pycodestyle.enabled" nil t) 
-				  ("pyls.plugins.mccabe.enabled" nil t) 
-				  ("pyls.plugins.pyflakes.enabled" nil t))) 
-  :hook ((python-mode . lsp) 
-	 (java-mode . lsp) 
-	 (lsp-mode . lsp-enable-which-key-integration)))
+  :config
+  (setq read-process-output-max (* 1024 1024)
+	lsp-idle-delay 0.5 ;; "Increase the amount of data which Emacs reads from the process. Again the emacs default is too low 4k considering that the some of the language server responses are in 800k - 3M range."
+	lsp-enable-symbol-highlighting t lsp-enable-snippet nil	;; Not supported by company capf, which is the recommended company backend
+	lsp-pyls-plugins-flake8-enabled t gc-cons-threshold 100000000 ;; "Adjust gc-cons-threshold. The default setting is too low for lsp-mode’s needs due to the fact that client/server communication generates a lot of memory/garbage."
+	lsp-headerline-breadcrumb-enable nil ;; Don't show headerline
+	lsp-completion-show-kind nil ;; Don't show completion kind
+	lsp-completion-show-detail nil
+	lsp-disabled-clients '(pylsp pyls)
+	) ;; Don't show Completion item detail
+  :hook ((java-mode . lsp) 
+	 (lsp-mode . lsp-enable-which-key-integration))
+  :bind (:map python-mode-map
+	      ("M-S-<right>" . python-indent-shift-right)
+	      ("M-S-<left>" . python-indent-shift-left)))
+
+
 
 ;; python
+(use-package lsp-pyright
+  :ensure t
+  :hook (python-mode . (lambda ()
+                          (require 'lsp-pyright)
+                          (lsp))))
+
 (use-package
   blacken
   :demand t
