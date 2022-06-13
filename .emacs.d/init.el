0da5a1b43bdfe97f0c74194b9a0f10bda0dbb815
Nishant <me@nishantjha.org>
2022-06-13 17:41:36 -0400

    trying out modus themes

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -32,7 +32,13 @@
 (use-package zenburn-theme 
   :ensure t
   :config
-  (load-theme 'zenburn t))
+  (load-theme 'zenburn t)
+  )
+
+;; (use-package modus-themes
+;;   :ensure t
+;;   :config
+;;   (load-theme 'modus-vivendi t))
 
 (use-package display-line-numbers
   :commands (display-line-numbers-mode)
@@ -536,3 +542,16 @@
 
 (projectile-mode 1)
 (message "reached end of init.el")
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("dad40020beea412623b04507a4c185079bff4dcea20a93d8f8451acb6afc8358" default)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(sp-show-pair-match-face ((t (:foreground "Gray")))))
