de40658d45a0f6e7c3254da262290298bc593047
Nishant <me@nishantjha.org>
2024-01-24 10:18:02 -0800

    documentation at point python

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -481,7 +481,7 @@
 (use-package swift-mode
   :ensure t
   :mode
-  (".swift")
+  ("\\.swift?\\'" . swift-mode)
   :hook (swift-mode . (lambda () (lsp)))
   :config
   (defun xcode-open-current-file()
@@ -518,7 +518,10 @@
   :ensure t
   :after lsp-mode
   :config
-  (setq lsp-sourcekit-executable (string-trim (shell-command-to-string "xcrun --find sourcekit-lsp"))))
+  (setq lsp-sourcekit-executable (string-trim (shell-command-to-string "xcrun --find sourcekit-lsp")))
+  :bind
+  (:map lsp-mode-map
+	("C-c d" . lsp-describe-thing-at-point)))
 
 
 ;; Go
@@ -555,11 +558,32 @@
   )
 
 ;; Python
+
+(defun my-lsp-describe-at-point ()
+  "Show LSP documentation in the minibuffer, with Markdown and HTML formatting stripped."
+  (interactive)
+  (let ((response (lsp-request "textDocument/hover" (lsp--text-document-position-params))))
+    (if response
+        (let ((contents-hash (gethash "contents" response)))
+          (if (and contents-hash (hash-table-p contents-hash))
+              (let ((doc-string (gethash "value" contents-hash)))
+                ;; Remove Markdown formatting and replace HTML entities
+		(setq doc-string (replace-regexp-in-string "```python" "" doc-string)) ; remove code block beginnning
+		(setq doc-string (replace-regexp-in-string "```" "" doc-string)) ; remove code block ending
+                (setq doc-string (replace-regexp-in-string "---" "" doc-string)) ; remove horizontal rules
+                (setq doc-string (replace-regexp-in-string "&nbsp;" " " doc-string)) ; replace &nbsp; with space
+                ;; Display processed string
+                (message "%s" (or doc-string "Documentation not available.")))
+            (message "Documentation not available.")))
+      (message "No response from LSP server."))))
+
 (use-package lsp-pyright
   :ensure t
   :hook (python-mode . (lambda ()
                          (require 'lsp-pyright)
                          (lsp-deferred)))
+  :bind (:map lsp-mode-map
+	      ("C-c d" . my-lsp-describe-at-point))
   :init
   (when (executable-find "python3")
     (setq lsp-pyright-python-executable-cmd "python3")))
